a0c3e1cb48aa375fd29b7984c9f405fd98c7c93e
bsimps01 <b.albertsimpson@gmail.com>
2021-07-16 00:53:05 -0700

    added slack connection with slackbot

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/bsimps01/MakeUtility
+
+go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/nlopes/slack v0.6.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/slack-go/slack v0.9.2 // indirect
+	github.com/your/go-slackbot v0.0.0-20170406203448-098fc320f1b3
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e
+//github.com/go-resty/resty v1.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	godotenv.Load()
+
+	args := os.Args[1:]
+	fmt.Println(args)
+
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	preText := "*Jenkins build has finished!*"
+	jenkinsURL := "*Build URL:*" + args[0]
+	buildResult := "*" + args[1] + "*"
+	buildNumber := "*" + args[2] + "*"
+	jobName := "*" + args[3] + "*"
+
+	if buildResult == "*SUCCESS*" {
+		buildResult = buildResult + " :white_check_mark:"
+	} else {
+		buildResult = buildResult + " :x:"
+	}
+
+	dividerSection1 := slack.NewDividerBlock()
+	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
+	preTextField := slack.NewTextBlockObject("mrkdwn", preText+"\n\n", false, false)
+	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails, false, false)
+
+	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+
+	msg := slack.MsgOptionBlocks(
+		preTextSection,
+		dividerSection1,
+		jenkinsBuildDetailsSection,
+	)
+
+	_, _, _, err := api.SendMessage(
+		"C028YGHJH6U",
+		msg,
+	)
+
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+}
